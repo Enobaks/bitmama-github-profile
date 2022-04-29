8f24d35699489203f6619255d3ee419a5b3e0d78
Emmanuel <enobakhare.emmanuel@gmail.com>
2022-04-29 15:37:37 +0100

    Initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="login-wrap">
+      <div className="input-container">
+        <button className="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
